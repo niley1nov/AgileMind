9fc24cb9348e8d10dc3472ecc6596c8be9fda5a6
Nilay Sharma <yalin.yhb@gmail.com>
2024-08-08 23:19:32 +0530

    helper

---
+++ server/helper/businessService.js
@@ -0,0 +1,24 @@
+import {
+    GoogleGenerativeAI
+  } from "@google/generative-ai";
+  import { models, getPrompts } from "../services/AIConfigData.js";
+  import config from "../config.js";
+
+  class businessService {
+    constructor(srs) {
+        this.genAI = new GoogleGenerativeAI(config.GEMINI_API_TOKEN);
+        this.model = this.genAI.getGenerativeModel({
+          model: models["pro"],
+          systemInstruction: getPrompts("business_model", [srs]),
+        });
+        this.chatSession = model.startChat({
+          generationConfig: getGenConfig(0.8, "text/plain"),
+          history: [],
+        });
+    }
+
+    async chat(question) {
+      const result = await this.chatSession.sendMessage(question);
+      return result.response.text();
+    }
+  }
