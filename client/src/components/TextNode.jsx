23b735870504c497f0e7c449f4c421cd83975089
Nilay Sharma <nilay.sharma@salesforce.com>
2024-08-09 17:27:05 +0530

    graph enhancements

--- client/src/components/TextNode.jsx
+++ client/src/components/TextNode.jsx
@@ -3,45 +3,13 @@ import { Handle, useStore, Position, useReactFlow } from "@xyflow/react";
 
 const TextNode = memo(function TextNode({ sourcePosition, targetPosition, data }) {
 	const { setNodes } = useReactFlow();
-	const dimensions = useStore((s) => {
-		const node = s.nodeLookup.get("2-3");
-
-		if (
-			!node ||
-			!node.width ||
-			!node.height ||
-			!s.edges.some((edge) => edge.target === data.id)
-		) {
-			return null;
-		}
-
-		return {
-			width: node.width,
-			height: node.height,
-		};
-	});
-
-	const updateDimension = (attr) => (event) => {
-		setNodes((nds) =>
-			nds.map((n) => {
-				if (n.id === "2-3") {
-					return {
-						...n,
-						style: {
-							...n.style,
-							[attr]: parseInt(event.target.value),
-						},
-					};
-				}
-
-				return n;
-			})
-		);
-	};
 
 	return (
 		<>
-			<div className="wrapper gradient">
+			<div
+				className={`wrapper gradient ${data.highlighted ? "highlighted" : "normal"
+					}`}
+			>
 				<div className="inner">
 					<label>{data.label}</label>
 					<input
